4d0dae4b5a71554f7803eee5ed9e8fc96ab3b5ae
Rayane <rchaabane20@gmail.com>
2023-09-02 22:46:14 +0200

    Arrows animation

---
+++ Script/script.js
@@ -0,0 +1,8 @@
+// Fonction pour actualiser la page toutes les 6 secondes
+function actualiserPage() {
+    location.reload();
+  }
+  
+  // Appeler la fonction toutes les 3 secondes
+  setInterval(actualiserPage, 3000); 
+  
